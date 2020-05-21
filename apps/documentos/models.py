83e2ec06721b8da69c8d1efd080c2176d6fc2536
Yuri Costa <yuri.sales@protonmail.com>
2020-05-21 17:20:22 -0300

    Add relationships

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from apps.funcionarios.models import Funcionario
+
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
